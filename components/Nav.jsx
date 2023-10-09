eb9437044b2e8c4d356fc7d4533089fe45a563fb
Ashan Dimantha <85834813+ashandimantha2000@users.noreply.github.com>
2023-10-10 03:16:53 +0530

    update: respnsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,19 +1,31 @@
 import Image from 'next/image';
 // import AiesecLogo from './Icons/bluelogo.png';
 import AiesecLogo from '../public/assets/images/bluelogo.png'
-import '../styles/globals.css';
-
-
+import '@styles/globals.css';
+import { useState } from 'react';
+import {HiMenuAlt3} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
 
 function Nav() {
+
+  const [showMenu, setShowMenu] = useState(false);
+  const toggle = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const toggleClose = () => {
+    setShowMenu(false);
+  };
+
   return (
-    <nav className=' grid grid-cols-3 h-20 bg-white fixed z-50 w-full'>
-      <div className='flex md:flex-row  flex-col justify-center items-center'>
-        <Image src={AiesecLogo} alt="AIESEC Logo" width={170} className='hidden lg:block items-center mt-3'/>
+    <nav id='mobile-nav' className=' grid grid-cols-3 h-20 bg-white fixed z-50 w-full'>
+      <div id='mobile-nav-logo' className='flex md:flex-row justify-center items-center'>
+        <Image src={AiesecLogo} alt="AIESEC Logo" width={170} className='hidden xsm:block z-50'/>
         {/* hidden lg:block */}
-        
+        <button className={showMenu ? 'toggle-btn-hide' : 'toggle-btn-show'} onClick={toggle}><HiMenuAlt3 size={'1.5rem'} color='#037Ef3' /></button>
+        <button className={showMenu ? 'toggle-btn-show' : 'toggle-btn-hide'} onClick={toggleClose}><AiOutlineClose size={'1.5rem'} color='#037Ef3' /></button>
       </div>
-      <div className='lg:static absolute bg-white lg:min-h-fit min-h-[60vh] left-0 top-[9%] lg:w-auto w-full  col-span-2'>
+      <div className='lg:static absolute bg-white lg:min-h-fit min-h-[60vh] left-0 top-[9%] lg:w-auto w-full  col-span-2' id={showMenu ? 'show' : 'hide'}>
         <ul className="flex lg:flex-row flex-col xsm:items-center md:gap-[4vw] gap-10 pt-6">
           <li>
             <a href="">Home</a>
